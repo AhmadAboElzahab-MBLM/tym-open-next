dc825e428929eb333c59da16564bd623d4e271c5
AhmadAboElzahab-MBLM <aelzahab@mblm.com>
2026-01-25 21:50:12 +0400

    update sitemap logic

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: `https://tym.world${process.env.NEXT_PUBLIC_LANG || ''}`,
+  siteUrl: `https://tym.world/${process.env.NEXT_PUBLIC_LANG || ''}`,
   generateRobotsTxt: false,
   changefreq: 'daily',
   priority: 0.7,
--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: `https://tym.world${process.env.NEXT_PUBLIC_LANG || ''}`,
+  siteUrl: `https://tym.world/${process.env.NEXT_PUBLIC_LANG || ''}`,
   generateRobotsTxt: true, // Generate robots.txt file alongside sitemap.xml
   changefreq: 'daily', // Change frequency of updates
   priority: 0.7, // Priority of URLs in sitemap
