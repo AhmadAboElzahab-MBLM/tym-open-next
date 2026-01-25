367ef3fbbe75263083637479356a5631dab3c16f
AhmadAboElzahab-MBLM <aelzahab@mblm.com>
2026-01-25 22:07:56 +0400

    remove sitemap library

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: `https://tym.world/${process.env.NEXT_PUBLIC_LANG || ''}`,
-  generateRobotsTxt: false,
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 7000,
-};
